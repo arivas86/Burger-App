2fb6f2891db234d65da80327d18b721445e143ef
Angel Rivas <angel@prodigycode.com>
2019-08-13 19:54:06 -0500

    updated hosting package for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host:'localhost',
-    port: 8889,
-    user:'root',
-    password:'root',
-    database:'burgers_db'
+    host:'r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+    port: 3306,
+    user:'io6i9p2rctkvdan0',
+    password:'nnisbo7angr8lvhr',
+    database:'hsfn2o3dq3jmtmcm'
 });
 
 connection.connect(function(err){
